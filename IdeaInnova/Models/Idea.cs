94440cd7a11509cd91aa93bd40f6ea1ef799ec16
RudraPatelhere <Rpatel1198@conestogac.on.ca>
2025-03-16 22:21:10 -0400

    updated model

---
+++ IdeaInnova/Models/Idea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdeaInnova.Models
+{
+    public class Idea
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }  // Nullable
+
+        [Required]
+        public string? Description { get; set; }  // Nullable
+
+        public DateTime SubmissionDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ IdeaInnova/Models/IdeaInnovaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IdeaInnova.Models
+{
+    public class IdeaInnovaContext : DbContext
+    {
+        public IdeaInnovaContext(DbContextOptions<IdeaInnovaContext> options) : base(options) { }
+
+        public DbSet<Idea> Ideas { get; set; }
+    }
+}
